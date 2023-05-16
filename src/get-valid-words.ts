d94831f6d27dcfac898862a62c146ca5b1283cab
Eric Jonas <ericdjonas@gmail.com>
2023-05-16 04:43:50 -0400

    Modified dictionary entries and format

--- src/get-valid-words.ts
+++ src/get-valid-words.ts
@@ -1,4 +1,4 @@
-import dictionary from './dictionary'
+import { dictionary } from './dictionary'
 
 export default function getValidWords(arr: string[]) {
   return (arr.length) ? arr.filter(str => isWord(str)) : []
@@ -6,7 +6,9 @@ export default function getValidWords(arr: string[]) {
 
 function isWord(str: string) {
   if (!str.length) return false
-  const entry = dictionary.find(({ firstLetter }) => firstLetter === str[0])
+
+  const strStart = str.length === 1 ? str : str[0] + str[1]
+  const entry = dictionary.find(({ start }) => start === strStart)
   
-  return entry?.words.includes(str)
+  return !!entry?.words.includes(str)
 }
